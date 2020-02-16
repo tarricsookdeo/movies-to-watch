505afeea1708c04a0d57023ff6c0a1cb1de41785
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-02-16 06:18:28 -0500

    Will create initial data pull in.

--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,20 @@
 const BASE_URL = "http://localhost:3000";
+
+window.addEventListener("load", () => {
+  getGenres();
+});
+
+const getGenres = () => {
+  url = BASE_URL + "/genres";
+
+  let genreList = document.getElementById("genre-list");
+  genreList.innerHTML = "";
+
+  fetch(url)
+    .then(response => response.json())
+    .then(genres => {
+      genres.forEach(genre => {
+        genreList.innerHTML += `name: ${genre.name}`;
+      });
+    });
+};
