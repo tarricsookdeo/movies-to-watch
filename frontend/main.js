e907e391537a27e459d38092cd739ae2cd983072
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-02-16 06:43:22 -0500

    Added function to generate genre markup.

--- frontend/main.js
+++ frontend/main.js
@@ -5,16 +5,30 @@ window.addEventListener("load", () => {
 });
 
 const getGenres = () => {
-  url = BASE_URL + "/genres";
+  const url = BASE_URL + "/genres";
 
-  let genreList = document.getElementById("genre-list");
+  let genreList = document.getElementById("accordion");
   genreList.innerHTML = "";
 
   fetch(url)
     .then(response => response.json())
     .then(genres => {
       genres.forEach(genre => {
-        genreList.innerHTML += `name: ${genre.name}`;
+        genreList.innerHTML += generateGenreMarkup(genre);
       });
     });
+};
+
+const generateGenreMarkup = genreObj => {
+  const markup = `<div class="card">
+        <div class="card-header" id="${genreObj.id}">
+            <h5 class="mb=0>
+                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    ${genreObj.name}
+                </button>
+            </h5>
+        </div>
+    </div>`;
+
+  return markup;
 };
