646d8a2bb09fdbe7bd8631ff015dec271514e84a
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-02-15 23:05:39 -0500

    Will create seed data.

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create seed genres
+genres = Genre.create([
+    { name: 'Horror' },
+    { name: 'Action' },
+    { name: 'Comedy' },
+    { name: 'animation' }
+])
+
+# Create seed movies
+movies = Movie.create([
+    { title: 'The Grudge', length: 96, is_watched: false, genre_id: 1 },
+    { title: 'John Wick', length: 101, is_watched: true, genre_id: 2 },
+    { title: 'Zombie Land', length: 99, is_watched: false, genre_id: 3 },
+    { title: 'Toy Story', length: 81, is_watched: true, genre_id: 4 },
+    { title: 'Good Boys', length: 90, is_watched: false, genre_id: 3 }
+])
