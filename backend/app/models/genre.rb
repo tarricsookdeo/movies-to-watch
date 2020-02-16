3bc4caace9d17c1456c5f75e0100ab3f9edbd117
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-02-15 22:51:05 -0500

    Will setup activerecord relationships in models.

--- backend/app/models/genre.rb
+++ backend/app/models/genre.rb
@@ -1,3 +1,5 @@
 class Genre < ApplicationRecord
     validates :name, uniqueness: true
+
+    has_many :movies, dependent: :destroy
 end
--- backend/app/models/movie.rb
+++ backend/app/models/movie.rb
@@ -1,2 +1,3 @@
 class Movie < ApplicationRecord
+    belong_to :genre
 end
