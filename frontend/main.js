6e4ae88e2522bafff7fc8dd5d2eaf6c431418b9f
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-02-17 00:53:56 -0500

    Will add ability to generate movie markup.

--- frontend/main.js
+++ frontend/main.js
@@ -1,4 +1,5 @@
 const BASE_URL = "http://localhost:3000";
+let data = [];
 
 window.addEventListener("load", () => {
   getGenres();
@@ -13,6 +14,8 @@ const getGenres = () => {
   fetch(url)
     .then(response => response.json())
     .then(genres => {
+      console.log(genres);
+      data = genres;
       genres.forEach(genre => {
         genreList.innerHTML += generateGenreMarkup(genre);
       });
@@ -20,15 +23,37 @@ const getGenres = () => {
 };
 
 const generateGenreMarkup = genreObj => {
-  const markup = `<div class="card">
-        <div class="card-header" id="${genreObj.id}">
-            <h5 class="mb=0>
-                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    ${genreObj.name}
-                </button>
-            </h5>
-        </div>
-    </div>`;
+  const genreMovies = genreObj.movies;
 
+  let genreMarkup = `<div class="card">
+                        <div class="card-header" id="${genreObj.name}">
+                            <h5 class="mb=0">
+                                <button class="btn btn-link"
+                                data-toggle="collapse"
+                                data-target="#${genreObj.id}"
+                                aria-expanded="true" 
+                                aria-controls="${genreObj.id}">
+                                    ${genreObj.name}
+                                </button>
+                            </h5>
+                        </div>
+                        
+                        <div id="${genreObj.id}" 
+                         class="collapse show" 
+                         aria-labelledby="${genreObj.name}" 
+                         data-parent="#accordion">
+                          <div class="card-body">
+                            <ul>
+                              ${generateMovieMarkup(genreMovies[0])}
+                            </ul>
+                          </div>
+                        </div>
+                      </div>`;
+
+  return genreMarkup;
+};
+
+const generateMovieMarkup = movieObj => {
+  const markup = `<li>Title: ${movieObj.title} - Length: ${movieObj.length} minutes - Watched: ${movieObj.is_watched}</li>`;
   return markup;
 };
